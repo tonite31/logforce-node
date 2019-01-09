a94090d4fe8bd9fed8dd6e4ace985e1e4efc10e1
rayim <rayim@creeta.com>
2019-01-09 16:47:06 +0900

    socket.io 어댑터 추가

---
+++ src/adapters/socket.io.adapter.js
@@ -0,0 +1,44 @@
+(function()
+{
+    const io = require('socket.io-client');
+
+    let SocketIOAdapter = function(url)
+    {
+        this.url = url;
+        this.socket = undefined;
+    };
+
+    SocketIOAdapter.prototype.connect = function(callback)
+    {
+        if(this.url)
+        {
+            if(!this.socket)
+            {
+                this.socket = io(this.url + '/logforce-stream');
+                this.socket.on('connect', function()
+                {
+                    callback();
+                });
+            }
+            else
+            {
+                callback();
+            }
+        }
+        else
+        {
+            throw new Error('[logforce] socket.io url is not defined');
+        }
+    };
+
+    SocketIOAdapter.prototype.publish = function(logger)
+    {
+        let self = this;
+        this.connect(function()
+        {
+            self.socket.emit('log', { log: logger.serialize() });
+        });
+    };
+
+    module.exports = SocketIOAdapter;
+})();
