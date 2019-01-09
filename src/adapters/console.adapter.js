a9f3fdd19a4aa0a3de3a365e27407fef746659e5
rayim <rayim@creeta.com>
2019-01-09 16:45:25 +0900

    콘솔 출력 방식 변경

---
+++ src/adapters/console.adapter.js
@@ -0,0 +1,65 @@
+(function()
+{
+    const chalk = require('chalk');
+
+    const ConsoleAdapter = function()
+    {
+        this.name = 'console';
+    };
+
+    ConsoleAdapter.prototype.publish = function(logger)
+    {
+        this.log(1, logger.logs);
+    };
+
+    ConsoleAdapter.prototype.log = function(tab, logs)
+    {
+        for(var i=0, l=logs.length; i<l; i++)
+        {
+            let log = JSON.parse(JSON.stringify(logs[i]));
+            if(Array.isArray(log))
+            {
+                this.log(tab + 1, log);
+            }
+            else
+            {
+                let log = logs[i];
+                var logFormat = '[ns:' + log.ns + ']';
+                for(let key in log.tags)
+                {
+                    logFormat += '[' + key + ':' + log.tags[key] + ']';
+                }
+
+                let options = log.options;
+
+                var color = '';
+                if(options && options.color)
+                {
+                    color = options.color;
+                }
+
+                let timestamp = log.timestamp;
+
+                let level = log.level;
+
+                var tabText = '';
+                for(var tt=1; tt<tab; tt++)
+                {
+                    tabText += '\t';
+                }
+
+                var logText = tabText + '[' + level + ']' + logFormat + '[ts:' + timestamp + '] ' + JSON.stringify(log.data);
+                if(color)
+                {
+                    console.log(chalk[color](logText));
+                }
+                else
+                {
+                    console.log(logText);
+                }
+            }
+        }
+    };
+
+    module.exports = ConsoleAdapter;
+})();
