cd1d31d4e0bcb4aff54f172dd731a93c11b6bc68
rayim <rayim@creeta.com>
2019-06-12 20:14:34 +0900

    fixed bugs

--- src/adapters/slack.adapter.js
+++ src/adapters/slack.adapter.js
@@ -13,12 +13,24 @@
 
     SlackAdapter.prototype.publish = function(logger)
     {
-        this.slack.webhook({
-            channel: this.options.channel,
-            username: this.options.username,
-            text: '```' + JSON.stringify(logger.json(), null, 4) + '```'
-        }, function(err, response) {
-        });
+        let json = logger.json();
+
+        for(let i=0; i<json.logs.length; i++)
+        {
+            if(!this.level || this.level === json.logs[i].level)
+            {
+                delete json.logs[i].level;
+
+                let log = { header: json.header, logs: json.logs[i] };
+
+                this.slack.webhook({
+                    channel: this.options.channel,
+                    username: this.options.username,
+                    text: '```' + JSON.stringify(log, null, 4) + '```'
+                }, function(err, response) {
+                });
+            }
+        }
     };
 
     module.exports = SlackAdapter;
