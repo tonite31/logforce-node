bacf901cf80da8646b9a83c0b453ec5c6d042987
Rayim <tonite32@gmail.com>
2019-07-06 22:42:16 +0900

    Update slack.test.js

--- test/slack.test.js
+++ test/slack.test.js
@@ -2,7 +2,7 @@ const Logforce = require('../index.js');
 
 (function()
 {
-    let logforce = new Logforce({ timestamp: { format: 'hh:mm:ss', timezone: 'asia/seoul' }, adapters: [new Logforce.Adapter.Slack({ webhookUrl: '', channel: '#104_개발서버로그', username: 'YOUR_USERNAME' }, 'error')] });
+    let logforce = new Logforce({ timestamp: { format: 'hh:mm:ss', timezone: 'asia/seoul' }, adapters: [new Logforce.Adapter.Slack({ webhookUrl: 'YOUR_WEBHOOK', channel: 'YOUR_CHANNEL', username: 'YOUR_USERNAME' }, 'error')] });
     let logger = logforce.createLogger({ test: 'test' });
 
     logger.add('log', { test: '일반적인 로그' });
